2ddf37e945a193237255763539f7c0b5ea523b27
PabloLec <pablo.lecolinet@gmail.com>
2021-06-14 15:13:16 +0200

    Update minified static files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,53 +1,52 @@
 module.exports = function (grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
 
-        concat: {
-            js: {
-                src: [
-                    'src/js/topbar.js',
-                    'src/js/fetcher.js',
-                    'src/js/carousel.js',
-                    'src/js/modal.js',
-                ],
-                dest: 'src/js/production.js',
-            },
-            css: {
-                src: [
-                    'src/css/carousel.css',
-                    'src/css/main.css',
-                    'src/css/modal.css',
-                ],
-                dest: 'src/css/production.css',
-            },
+    concat: {
+      common_js: {
+        src: [
+          "oc_lit_review/static/assets/jquery/jquery-3.6.0.min.js",
+          "oc_lit_review/static/assets/bootstrap/bootstrap.min.js",
+        ],
+        dest: "oc_lit_review/static/assets/js/common.js",
+      },
+      common_css: {
+        src: [
+          "oc_lit_review/static/assets/bootstrap/bootstrap.min.css",
+          "oc_lit_review/static/assets/css/style.css",
+          "oc_lit_review/static/assets/css/navbar.css",
+          "oc_lit_review/static/assets/fontawesome/css/all.css",
+        ],
+        dest: "oc_lit_review/static/assets/css/common.css",
+      },
+    },
+    uglify: {
+      common_js: {
+        src: "oc_lit_review/static/assets/js/common.js",
+        dest: "oc_lit_review/static/assets/js/min/common.min.js",
+      },
+    },
+    cssmin: {
+      css: {
+        src: "oc_lit_review/static/assets/css/common.css",
+        dest: "oc_lit_review/static/assets/css/min/common.min.css",
+      },
+    },
+    watch: {
+      scripts: {
+        files: ["src/js/*.js", "src/css/*.css"],
+        tasks: ["concat", "uglify", "cssmin"],
+        options: {
+          spawn: false,
         },
-        uglify: {
-            build: {
-                src: 'src/js/production.js',
-                dest: 'src/js/production.min.js',
-            },
-        },
-        cssmin: {
-            css: {
-                src: 'src/css/production.css',
-                dest: 'src/css/production.min.css',
-            },
-        },
-        watch: {
-            scripts: {
-                files: ['src/js/*.js', 'src/css/*.css'],
-                tasks: ['concat', 'uglify', 'cssmin'],
-                options: {
-                    spawn: false,
-                },
-            },
-        },
-    })
+      },
+    },
+  });
 
-    grunt.loadNpmTasks('grunt-contrib-concat')
-    grunt.loadNpmTasks('grunt-contrib-uglify')
-    grunt.loadNpmTasks('grunt-contrib-cssmin')
-    grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-contrib-watch");
 
-    grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'watch'])
-}
+  grunt.registerTask("default", ["concat", "uglify", "cssmin"]);
+};
